60918a49b96d93fb3956a930eea0a485dc46f586
Himan10 <h3c.initiative46@gmail.com>
2023-03-13 19:21:52 +0530

    Added a listener to accept messages from web extension

--- extension/injectJSsnippet.js
+++ extension/injectJSsnippet.js
@@ -1,3 +1,19 @@
 const scriptSrc = document.createElement("script")
 scriptSrc.setAttribute("src", "/GrammarlyAlternate/extension/eventListener.js")
-document.body.append(scriptSrc)
+scriptSrc.setAttribute("id", "script#01")
+// onMessage.addListener expects a callback function as an argument's value.
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	try {
+		message = request.message.trim()
+		codeInjectStatus = message.split('=')[1].toLowerCase()
+		if (codeInjectStatus == "true") {
+			document.body.append(scriptSrc)
+		} else if (codeInjectStatus == "false") {
+			const scriptTag = document.getElementById("script#01")
+			document.body.removeChild(scriptTag)
+		}
+	} catch (e) {
+		console.error("Something Went Wrong", e.message);
+	}
+});
+
