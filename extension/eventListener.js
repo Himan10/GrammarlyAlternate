6e1de55d3abac99bcbaac600867fe0ef94379ecf
Himan10 <h3c.initiative46@gmail.com>
2023-03-14 22:10:12 +0530

    Modified the code base for better interface of the dialogBox

--- extension/eventListener.js
+++ extension/eventListener.js
@@ -18,17 +18,13 @@ function highlightText() {
 
 				// create the pop up box for displaying the info.
 				const popUpBox = createElement([shortDesc, longDesc])
-
-				// get the coordinates of highlighted element
-				const coordinates = highlightedText.getRangeAt(0).getBoundingClientRect()
-
         			document.body.append(popUpBox)
-				//popUpBox.showModal(); // to display the pop up
+				popUpBox.style.display = "block" // to display the pop up
                         }
                 };
                 xhr.send()
         } else {
-		const popUpBox = document.getElementById("div0")
+		const popUpBox = document.getElementById("div1")
 		if (popUpBox) {
 			popUpBox.style.display = "none" //change to "block" to redisplay it.
 			document.body.removeChild(popUpBox)
@@ -36,33 +32,58 @@ function highlightText() {
 	}
 }
 
+
 function runEvent() {
         document.addEventListener("mouseup", function() {
                 highlightText();
         });
 }
 
+function createLinkTag(link, file) {
+	link = document.createElement("link")
+	link.setAttribute("rel", "stylesheet")
+	link.setAttribute("href", `${file}`)
+	return link
+}
+
 function createElement(data) {
 	// data should be in the form of {key: value}; dictionary
-	const div0 = document.createElement("div")
-	div0.setAttribute("id", "div0")
+	
+	const head = document.getElementsByTagName("head")[0]
+	const cssFileLocation = "http://localhost/GrammarlyAlternate/extension/popUpStyle.css"
+	var linkSrc = head.querySelector("link") 
+	if (!linkSrc) {
+		linkSrc = createLinkTag(linkSrc, cssFileLocation)
+		document.head.appendChild(linkSrc)
+	} else if (linkSrc.getAttribute("href") != cssFileLocation) {
+		linkSrc = createLinkTag(linkSrc, cssFileLocation)
+		document.head.appendChild(linkSrc)
+	}
+
 	const div1 = document.createElement("div")
-	div1.setAttribute("style", "overflow: auto; width: 300px; height: 300px")
-	div1.setAttribute("id", "div01")
+	div1.setAttribute("id", "div1")
+	div1.setAttribute("class", "modal")
+
 	const div2 = document.createElement("div")
 	div2.setAttribute("class", "popUpContent")
+	div2.setAttribute("style", "width: 300px; height: 300px; margin-left: 990px; margin-top: 10px;")
+	
+	const span = document.createElement("span")
+	span.setAttribute("class", "close")
+	span.innerText = "\u00D7" // unicode of &terms; which represent 'x' symbol
+	
 	const pElement = document.createElement("p")
-	//pElement.setAttribute("style", "width: 300px; height: 350px")
+	
 	const label = document.createElement("label")
 	for (const key of data) {
 		label.append(key)
 		label.innerHTML += "<br>"
 	}
 	pElement.appendChild(label)
+	div2.appendChild(span)
 	div2.appendChild(pElement)
 	div1.appendChild(div2)
-	div0.appendChild(div1)
-	return div0
+	return div1
 }
 
 runEvent();
